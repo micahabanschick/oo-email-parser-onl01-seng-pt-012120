0d4239f9139d37a6ae145877837b7096b89eddf2
Micah Banschick <micha.ban@gmail.com>
2020-02-03 19:01:23 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailAddressParser
   end 
   
   def parse 
-    arr = @emails.split(", ").each{|elem| elem.split(" ")}
+    arr = @emails.split(", ").map{|elem| elem.split(" ")}
     arr
   end 
     
